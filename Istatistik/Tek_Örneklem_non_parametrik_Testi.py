2621ca3da2ec52cac852979ac503e01869bc68d9
gorkenvm <64047117+gorkenvm@users.noreply.github.com>
2022-04-20 16:01:15 +0300

    Delete Tek_Örneklem_non_parametrik_Testi.py

--- Istatistik/Tek_Örneklem_non_parametrik_Testi.py
+++
@@ -1,6 +0,0 @@
-# Kullanacağımız örnek Normal dağılım yani parametrik bu yüzden sonucu dikkate alma. Sadece nasıl yapıldığı gösteriliyor.
-# Nonparametrik olunca bunu yapabiliyoruz.
-from statsmodels.stats.descriptivestats import sign_test
-from Tek_orneklem_T_Testi import olcumler                # bir daha yazmak yerine olcumler'i Tek_orneklem_T_Testi script'inden import ettik
-print(sign_test(olcumler, 170)) # Result (-7.0, 0.06490864707227217)
-# p value 0.05 ten büyük olduğu için Ho'ı reddedemeyecektik.
