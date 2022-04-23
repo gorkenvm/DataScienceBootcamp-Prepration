7305f77fe105788e06b607b57ecbd857f8ca96e3
dasci <gorkenvm@gmail.com>
2022-04-23 14:24:39 +0300

    go ahead

---
+++ Python/test.py
@@ -0,0 +1,15 @@
+
+import HesapModulu 
+
+HesapModulu.yeni_maas(1000)
+
+HesapModulu.yeni_maas(2000)
+
+import HesapModulu as hm
+hm.yeni_maas(1000)
+
+from HesapModulu import yeni_maas
+yeni_maas(4000)
+
+import HesapModulu as hm
+hm.maaslar
