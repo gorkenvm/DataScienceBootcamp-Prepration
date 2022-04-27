a21b778192e1f2c9da8f50c4fef37e60242ca3db
dasci <64047117+gorkenvm@users.noreply.github.com>
2022-04-27 11:05:06 +0300

    İndex eleman erisme

---
+++ VeriManipulasyonu/6_Index_ıle_elemana_erısme.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.random.randint(10, size = 10 )
+
+print(a[0], '\n', a[1], '\n',a[-1])  # bu şekilde istedigimiz indexi çekebiliriz.
+print("--------------------------------------")
+m =  np.random.randint((10, size = (3,5))
+print(m)
+print("--------------------------------------")
+print(m[0,0])           # 0. row 0. col
+print("--------------------------------------")
+m[1,4] = 99             # değer atama
+print("--------------------------------------")
+
+
+
