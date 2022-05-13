c462adf776708d853d74de8b4ab3e264da6ddcfc
dasci <64047117+gorkenvm@users.noreply.github.com>
2022-05-13 21:55:42 +0300

    Update poisson.py

--- Istatistik/poisson.py
+++ Istatistik/poisson.py
@@ -1,3 +1,13 @@
+# Poisson : Belirli bir zaman aralığında belirli bir alanda nadiren rastlanan olayların olayların olasılıklarını hesaplamak için kullanılır
+# Nadiren nedir ? 10.000 kelimeli kitapta hata sayısı, KK işlemlerinde sahtekarlık olayı, Rötara düşen uçuş sefer sayısı. 
+# n gözlem sayısı, p olasılık. nadir kabul edilmesi için n > 50, n*p < 5 olmalıdır. n*p Lambdadır.
+# Formülde Lambda var değiştirebiliyoruz, Lambda ortalama ve varansı ifade eder, Lambda artarsa yayılım artar.
+# Kesikli bir rastsal dağılımdır.
+# Binom dağılımının özel bir halidir.
+
+
+# İş Uygulaması
+# İlan giriş hata olasılıklarının hesaplanması
 import imp
 from scipy.stats import poisson
 # Lambda is 0.1 dir. 
