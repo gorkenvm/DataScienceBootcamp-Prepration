229f5bd0b8e4916d55adf09b4d8ffe4128262757
dasci <64047117+gorkenvm@users.noreply.github.com>
2022-05-13 19:57:13 +0300

    Update 3_güven_aralığı.py

--- Istatistik/3_güven_aralığı.py
+++ Istatistik/3_güven_aralığı.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 # Örneğin websitesinde geçirilen ortalama süre 180 sn. %95 olasılıkla web sitesinin güven aralığı 172-188 snyedir. Yani ortalamanın etrafındaki %95 agırlıklı süreyi de belirtmiş oluyoruz.
+# Yani 100 denemenin 95'i bu aralıkta olacaktır.
 
 fiyatlar = np.random.randint(10,110,1000)
 print(fiyatlar.mean())
