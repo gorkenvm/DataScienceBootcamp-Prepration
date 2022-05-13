5da66c1734650b12e1c5ad866665daa8d2ddff7b
dasci <64047117+gorkenvm@users.noreply.github.com>
2022-05-13 10:26:28 +0300

    Update güven_aralığı.py

--- Istatistik/güven_aralığı.py
+++ Istatistik/güven_aralığı.py
@@ -5,7 +5,8 @@ print(fiyatlar.mean())
 
 import statsmodels.stats.api as sms
 
-print(sms.DescrStatsW(fiyatlar).tconfint_mean()) # Güven aralığını bulmamıza yardımcı olur. Kabul edilen Güven aralığı %95'tir.
+print(sms.DescrStatsW(fiyatlar).tconfint_mean()) # Güven aralığını bulmamıza yardımcı olur. Kabul edilen Güven aralığı %95'tir. 
+# 56 ile 60 diye iki sonuç çıkarıyor, müşterilerin %95 güvenirlilikle 56-60 arası ödeme yaparak bu ürünü satın almak isteyeceğini söylüyor.
 
 def yazdir(metin):
     print(metin, "program öğrenilecek")
